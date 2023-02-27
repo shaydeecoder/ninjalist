8ecb22dcff587cdf02c3a0b9fd6a6a5a3eae76b0
shaydeecoder <shaydeecoder@gmail.com>
2023-02-27 09:03:02 +0100

    feat: Fetch single item

--- pages/ninjas/[id].js
+++ pages/ninjas/[id].js
@@ -1,4 +1,4 @@
-const getStaticPaths = async () => {
+export const getStaticPaths = async () => {
   const res = await fetch("https://jsonplaceholder.typicode.com/users");
   const data = await res.json();
 
@@ -14,10 +14,23 @@ const getStaticPaths = async () => {
   };
 };
 
-const Details = () => {
+export const getStaticProps = async (context) => {
+  const id = context.params.id;
+  const res = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
+  const data = await res.json();
+
+  return {
+    props: { ninja: data },
+  };
+};
+
+const Details = ({ ninja }) => {
   return (
     <div>
-      <h1>Details Page</h1>
+      <h1>{ninja.name}</h1>
+      <p>{ninja.email}</p>
+      <p>{ninja.website}</p>
+      <p>{ninja.address.city}</p>
     </div>
   );
 };
