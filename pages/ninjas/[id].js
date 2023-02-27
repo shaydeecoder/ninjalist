34595bb722fb85c0dd22375bd1c2904030ba4dd9
shaydeecoder <shaydeecoder@gmail.com>
2023-02-27 08:21:33 +0100

    feat: Dynamic routes - getStaticPaths

--- pages/ninjas/[id].js
+++ pages/ninjas/[id].js
@@ -1,3 +1,19 @@
+const getStaticPaths = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+
+  const paths = data.map((el) => {
+    return {
+      params: { id: el.id.toString() },
+    };
+  });
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
 const Details = () => {
   return (
     <div>
